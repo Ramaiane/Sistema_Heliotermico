5dfcc54d97cafe4455d6967adc44938fd2063e22
Ramaiane <Ramaiane@pc-ramaiane>
2014-05-30 22:58:40 -0300

    relatorio temperatura finalizado

--- controle/temperaturaControle.php
+++ controle/temperaturaControle.php
@@ -10,17 +10,42 @@ define('FPDF_FONTPATH', 'fpdf/font/');
 require '../fpdf17/fpdf.php';
 include '../modelo/pdo.php';
 
+class PDF extends FPDF{
+    function Header(){
+        //cabecalho do relatorio
+        $this->SetFont('Arial', '', 16);
+        $this->Cell(0, 0.5, "Relatorio Temperatura", 0,1, 'C');
+        $this->Cell(0,0.5, "", "B", 1,'C');
+        $this->Ln(1);
+    }
+    
+    function Footer() {
+        $this->SetY(-1.5);
+        
+        $this->SetFont('arial','I',9);
+         $this->Cell(2.5, 0.5,'Sistema Heliotermico - FGA', 'T', 0, 'L');
+         $this->Cell(2.5, 0.5,'',0,0,'L');
+         $this->Cell(2.5, 0.5,'Monitoramento Remoto', 'T', 0, 'L');
+         $this->Cell(4, 0.5,'',0,0,'L');
+         $this->SetFont('arial', '', 7);
+         $this->Cell(10,0, 'Pagina'.$this->PageNo().'', 0,1,'C');
+    }
+    
+}
 
-$fpdf = new FPDF('P', 'cm', 'A4');
-$fpdf->Open();
+
+$fpdf = new PDF('P', 'cm', 'A4');
+$fpdf->AliasNbPages();
+//$fpdf->Open();
 $fpdf->AddPage();
 
-//cabecalho
-$fpdf->SetFont('Arial', '', 16);
-$fpdf->Cell(0, 0.5, "Relatorio Temperatura", 0,1, 'C');
-$fpdf->Cell(0,0.5, "", "B", 1,'C');
-$fpdf->Ln(1);
-
+//cabecalho da tabela
+$fpdf->SetTextColor(0,0,0);
+$fpdf->SetFont('Arial', '', 12);
+$fpdf->Cell(5,1, "Temperatura", 1,0,'C');
+$fpdf->Cell(5,1, "Hora Monitoramento", 1,0,'C');
+$fpdf->Cell(5,1, "Data Monitoramento", 1,1,'C');
+     
 //linhas da tabela
 $fpdf->SetFont('Arial','', 11);
 
@@ -44,11 +69,12 @@ $fpdf->SetFont('Arial','', 11);
      
             while($row = $resultsquery->fetch()){
 
-                $fpdf->Cell(3, 1, $row->temperatura, 1, 0, 'C');
-                $fpdf->Cell(3, 1, $row->hora, 1, 0, 'C');
-                $fpdf->Cell(3, 1, $row->data_monitoramento, 1, 1, 'C');
+                $fpdf->Cell(5, 1, $row->temperatura, 1, 0, 'C');
+                $fpdf->Cell(5, 1, $row->hora, 1, 0, 'C');
+                $fpdf->Cell(5, 1, $row->data_monitoramento, 1, 1, 'C');
+  
                            
-             }
+             }  
   $fpdf->Output(); 
     
 /*
